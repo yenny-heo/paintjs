fd79ac3fc2d35e2d8005fe8936798efd4d845cf9
yenny <aldald73@naver.com>
2021-02-08 23:43:10 +0900

    feat: 브러쉬 크기 변경, 모드 토글 버튼

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +11,7 @@ ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -37,8 +40,7 @@ if (canvas) {
   canvas.addEventListener('mouseleave', stopPainting);
 }
 
-console.log(Array.from(colors));
-
+//색깔 변경
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
@@ -47,3 +49,29 @@ function handleColorClick(event) {
 Array.from(colors).forEach((color) =>
   color.addEventListener('click', handleColorClick),
 );
+
+//브러쉬 크기 변경
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+if (range) {
+  range.addEventListener('input', handleRangeChange);
+}
+
+//mode
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = 'FILL';
+  } else {
+    filling = true;
+    mode.innerText = 'PAINT';
+  }
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick);
+}
